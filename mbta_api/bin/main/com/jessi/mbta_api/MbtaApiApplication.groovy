9ed212150e7d1c151ad003bc5e9f078713253c02
Jessi Abrahams <jessi.abrahams@gmail.com>
2018-10-09 18:45:37 -0400

    initial code

---
+++ mbta_api/bin/main/com/jessi/mbta_api/MbtaApiApplication.groovy
@@ -0,0 +1,17 @@
+package com.jessi.mbta_api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MbtaApiApplication {
+
+	static void main(String[] args) {
+		//SpringApplication.run MbtaApiApplication, args
+		println("hey")
+
+		ObjectMapper ObjectMapper = new ObjectMapper();
+
+
+	}
+}
---
+++ mbta_api/src/main/groovy/com/jessi/mbta_api/Route.groovy
@@ -0,0 +1,23 @@
+@Type('route')
+class Route {
+
+  @Id
+  String id
+
+  int Type
+
+  String textColor
+
+  int sortOrder
+
+  String shortName
+
+  String longName
+
+  String directionNames
+
+  String description
+
+  String color
+
+}
