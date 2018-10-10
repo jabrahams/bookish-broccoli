32f454a72578ad15aebb69acadb42db5791b11c7
Jessi Abrahams <jessi.abrahams@gmail.com>
2018-10-10 12:16:14 -0400

    try java

---
+++ mbta_api/src/main/java/com/jessi/mbta_api/MbtaApiApplication.java
@@ -0,0 +1,12 @@
+package com.jessi.mbta_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtaApiApplication.class, args);
+	}
+}
